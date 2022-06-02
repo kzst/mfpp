5dd0b175bc65127d4287d7da49b25443d94e2064
kzst <kzst@gtk.uni-pannon.hu>
2022-06-02 14:41:24 +0200

    mfpp_0.0.4

--- R/generatepdm.r
+++ R/generatepdm.r
@@ -28,82 +28,126 @@ generatepdm<- function(N,ff,cf,mTD,mCD,mRD,w,nR,nW,scale=1.4,QD=FALSE,lst=FALSE)
 
   if(missing(scale))
   {scale <- 1.4}
-if(!QD) {
-  cf <- cf+1
-  PEM <- phase3(pracma::triu((pracma::triu(pmin(pracma::ones(N)/pmax(pracma::repmat((1-cf):(N-cf),N,1)^(scale)-
-      (pracma::repmat(matrix(0:(N-1)),1,N)^(scale)), pracma::ones(N)),pracma::ones(N)),1) > pracma::rand(N))*1,1)+pracma::eye(N),ff,-0.5)   # Generate PEM matrix
-nTD <- w                          #Width of TD = number of modes
-nCD <- w                          #Width of CD = number of modes
-nRD <- w*nR                       #Width of RD = number of modes x number of resources
-TD <- pracma::rand(N,nTD)*mTD             #Generate time domain
-CD <- pracma::rand(N,nCD)*mCD             #Generate cost domain
-rD <- pracma::rand(N,nRD)*mRD             #Generate resource domain
-pem <- pracma::zeros(N+nW)
-pem[1:N,1:N]<- PEM
-td <- pracma::zeros(N+nW,nTD)
-cd <- pracma::zeros(N+nW,nCD)
-rd <- pracma::zeros(N+nW,nRD)
-if (w==2)                       #In case of CTCTP the columns will be sorted
+  if(!QD) {
+    cf <- cf+1
+    PEM <- phase3(pracma::triu((pracma::triu(pmin(pracma::ones(N)/
+            pmax(pracma::repmat((1-cf):(N-cf),N,1)^(scale)-
+                        (pracma::repmat(matrix(0:(N-1)),1,N)^(scale)),
+                 pracma::ones(N)),pracma::ones(N)),1) > pracma::rand(N))*1,1)+
+              pracma::eye(N),ff,-0.5)   # Generate PEM matrix
+    nTD <- w                          #Width of TD = number of modes
+    nCD <- w                          #Width of CD = number of modes
+    nRD <- w*nR                       #Width of RD = number of modes x number of
+                                      #resources
+    TD <- pracma::rand(N,nTD)*mTD             #Generate time domain
+    CD <- pracma::rand(N,nCD)*mCD             #Generate cost domain
+    rD <- pracma::rand(N,nRD)*mRD             #Generate resource domain
+    pem <- pracma::zeros(N+nW)
+    pem[1:N,1:N]<- PEM
+    td <- pracma::zeros(N+nW,nTD)
+    cd <- pracma::zeros(N+nW,nCD)
+    rd <- pracma::zeros(N+nW,nRD)
+    if (w==2)                       #In case of CTCTP the columns will be sorted
     {
-TD <- cbind(matrix(Rfast::rowMaxs(TD, value=TRUE))-matrix(Rfast::rowMaxs(TD, value=TRUE))*pracma::rand(N,1)*0.2,matrix(Rfast::rowMaxs(TD, value=TRUE)))
-CD <- cbind(matrix(Rfast::rowMaxs(CD, value=TRUE))-matrix(Rfast::rowMaxs(CD, value=TRUE))*pracma::rand(N,1)*0.2,matrix(Rfast::rowMaxs(TD, value=TRUE)))
-RD <- c()
-    for (i in seq(1,nRD,2)){
+      TD <- cbind(matrix(Rfast::rowMaxs(TD, value=TRUE))-
+                matrix(Rfast::rowMaxs(TD, value=TRUE))*
+                pracma::rand(N,1)*0.2,matrix(Rfast::rowMaxs(TD, value=TRUE)))
+      CD <- cbind(matrix(Rfast::rowMaxs(CD, value=TRUE))-
+                matrix(Rfast::rowMaxs(CD, value=TRUE))*
+                pracma::rand(N,1)*0.2,matrix(Rfast::rowMaxs(TD, value=TRUE)))
+      RD <- c()
+      for (i in seq(1,nRD,2)){
         rmax <- matrix(Rfast::colMaxs(t(rD[,i:(i+1)]),value=TRUE))
         rmax <- na.omit(rmax)
         rmin <- rmax-rmax*pracma::rand(N,1)*0.2
         rmin <- na.omit(rmin)
         RD <- cbind(RD,rmin,rmax) }
-} else {
-    RD <- rD
+    } else {
+      RD <- rD
     }
-td[1:N,1:nTD] <- TD
-cd[1:N,1:nCD] <- CD
-rd[1:N,1:nRD] <- RD
-PDM <- cbind(pem,td,cd,rd)              #output<- list(PDM=PDM, w=w)
-output$PDM<- PDM
-output$w <- w
-} else {
-  cf=cf+1
-  PEM=phase3(pracma::triu((pracma::triu(pmin(pracma::ones(N)/pmax(pracma::repmat((1-cf):(N-cf),N,1)^(scale)-
-    (pracma::repmat(matrix(0:(N-1)),1,N)^(scale)),pracma::ones(N)),pracma::ones(N)),1) > pracma::rand(N))*1,1)+pracma::eye(N),ff,-0.5)   # Generate PEM matrix
-  nTD=w                          #Width of TD = number of modes
-  nCD=w                          #Width of CD = number of modes
-  nQD=w                          #Width of QD = number of modes
-  nRD=w*nR                       #Width of RD = number of modes x number of resources
-  TD=pracma::rand(N,nTD)*mTD             #Generate time domain
-  CD=pracma::rand(N,nCD)*mCD             #Generate cost domain
-  QD=pracma::rand(N,nQD)                 #Generate quality domain
-  rD=pracma::rand(N,nRD)*mRD             #Generate resource domain
-  pem=pracma::zeros(N+nW)
-  pem[1:N,1:N]=PEM
-  td=pracma::zeros(N+nW,nTD)
-  cd=pracma::zeros(N+nW,nCD)
-  qd=pracma::zeros(N+nW,nQD)
-  rd=pracma::zeros(N+nW,nRD)
-  if (w==2)                       #In case of CTCTP the columns will be sorted
-  {
-    TD=cbind(matrix(Rfast::rowMaxs(TD, value=TRUE))-matrix(Rfast::rowMaxs(TD, value=TRUE))*pracma::rand(N,1)*0.2,matrix(Rfast::rowMaxs(TD, value=TRUE)))
-    CD=cbind(matrix(Rfast::rowMaxs(CD, value=TRUE))-matrix(Rfast::rowMaxs(CD, value=TRUE))*pracma::rand(N,1)*0.2,matrix(Rfast::rowMaxs(CD, value=TRUE)))
-    QD=cbind(matrix(Rfast::rowMaxs(QD, value=TRUE))-matrix(Rfast::rowMaxs(QD, value=TRUE))*pracma::rand(N,1)*0.2,matrix(Rfast::rowMaxs(QD, value=TRUE)))
-    RD=c()
-    for (i in seq(1,nRD,2)){
-      rmax=matrix(Rfast::colMaxs(t(rD[,i:(i+1)]),value=TRUE))
-      rmax=na.omit(rmax)
-      rmin=rmax-rmax*pracma::rand(N,1)*0.2
-      rmin=na.omit(rmin)
-      RD=cbind(RD,rmin,rmax) }
+    td[1:N,1:nTD] <- TD
+    cd[1:N,1:nCD] <- CD
+    rd[1:N,1:nRD] <- RD
+    PDM <- cbind(pem,td,cd,rd)
+
+    Rs<-nR
+    rownames(PDM)<-paste("a",1:nrow(PDM),sep="_")
+    if (Rs>0){
+      colnames(PDM)<-c(paste("a",1:nrow(PDM),sep='_'),
+                       paste("t",1:w,sep='_'),paste("c",1:w,sep='_'),
+                       paste(paste("r",1:w,sep='_'),rep(1:Rs,each=w),sep='.'))
+    }else{
+      colnames(PDM)<-c(paste("a",1:nrow(PDM),sep='_'),
+                       paste("t",1:w,sep='_'),paste("c",1:w,sep='_'))
+    }
+
+    class(PDM)<-"PDM_matrix"
+    output$PDM<- PDM
+    output$w <- w
   } else {
-    RD=rD
+    cf=cf+1
+    PEM=phase3(pracma::triu((pracma::triu(pmin(pracma::ones(N)/
+                      pmax(pracma::repmat((1-cf):(N-cf),N,1)^(scale)-
+          (pracma::repmat(matrix(0:(N-1)),1,N)^(scale)),pracma::ones(N)),
+          pracma::ones(N)),1) > pracma::rand(N))*1,1)+pracma::eye(N),ff,-0.5)
+    # Generate PEM matrix
+    nTD=w                          #Width of TD = number of modes
+    nCD=w                          #Width of CD = number of modes
+    nQD=w                          #Width of QD = number of modes
+    nRD=w*nR                       #Width of RD = number of modes
+                                   #x number of resources
+    TD=pracma::rand(N,nTD)*mTD             #Generate time domain
+    CD=pracma::rand(N,nCD)*mCD             #Generate cost domain
+    QD=pracma::rand(N,nQD)                 #Generate quality domain
+    rD=pracma::rand(N,nRD)*mRD             #Generate resource domain
+    pem=pracma::zeros(N+nW)
+    pem[1:N,1:N]=PEM
+    td=pracma::zeros(N+nW,nTD)
+    cd=pracma::zeros(N+nW,nCD)
+    qd=pracma::zeros(N+nW,nQD)
+    rd=pracma::zeros(N+nW,nRD)
+    if (w==2)                       #In case of CTCTP the columns will be sorted
+    {
+      TD=cbind(matrix(Rfast::rowMaxs(TD, value=TRUE))-
+                 matrix(Rfast::rowMaxs(TD, value=TRUE))*
+                 pracma::rand(N,1)*0.2,matrix(Rfast::rowMaxs(TD, value=TRUE)))
+      CD=cbind(matrix(Rfast::rowMaxs(CD, value=TRUE))-
+                 matrix(Rfast::rowMaxs(CD, value=TRUE))*
+                 pracma::rand(N,1)*0.2,matrix(Rfast::rowMaxs(CD, value=TRUE)))
+      QD=cbind(matrix(Rfast::rowMaxs(QD, value=TRUE))-
+                 matrix(Rfast::rowMaxs(QD, value=TRUE))*
+                 pracma::rand(N,1)*0.2,matrix(Rfast::rowMaxs(QD, value=TRUE)))
+      RD=c()
+      for (i in seq(1,nRD,2)){
+        rmax=matrix(Rfast::colMaxs(t(rD[,i:(i+1)]),value=TRUE))
+        rmax=na.omit(rmax)
+        rmin=rmax-rmax*pracma::rand(N,1)*0.2
+        rmin=na.omit(rmin)
+        RD=cbind(RD,rmin,rmax) }
+    } else {
+      RD=rD
+    }
+    td[1:N,1:nTD]=TD
+    cd[1:N,1:nCD]=CD
+    qd[1:N,1:nQD]=QD
+    rd[1:N,1:nRD]=RD
+    PDM=cbind(pem,td,cd,qd,rd)
+    Rs<-nR
+    rownames(PDM)<-paste("a",1:nrow(PDM),sep="_")
+    if (Rs>0){
+      colnames(PDM)<-c(paste("a",1:nrow(PDM),sep='_'),
+                       paste("t",1:w,sep='_'),paste("c",1:w,sep='_'),
+                       paste("q",1:w,sep='_'),
+                       paste(paste("r",1:w,sep='_'),rep(1:Rs,each=w),sep='.'))
+    }else{
+      colnames(PDM)<-c(paste("a",1:nrow(PDM),sep='_'),
+                       paste("t",1:w,sep='_'),paste("c",1:w,sep='_'),
+                       paste("q",1:w,sep='_'))
+    }
+    class(PDM)<-"PDM_matrix"
+    output$PDM<-PDM
+    output$w<-w
   }
-  td[1:N,1:nTD]=TD
-  cd[1:N,1:nCD]=CD
-  qd[1:N,1:nQD]=QD
-  rd[1:N,1:nRD]=RD
-  PDM=cbind(pem,td,cd,qd,rd)
-  output$PDM<- PDM
-  output$w <- w
-}
   class(PDM)<-"PDM_matrix"
   if (lst==FALSE){
     return(PDM)
